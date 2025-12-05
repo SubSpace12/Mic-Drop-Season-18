9447c992e058e88bd4593f18ae47b2761693fa3f
SubSpace12 <bpawelczyk2005@gmail.com>
2025-12-05 11:33:49 +0100

    test commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css',
+                 'resources/js/app.js',
+                'resources/css/admin.css',
+            'resources/css/dashboard.css',
+            'resources/css/judging.css',
+            'resources/css/results.css',
+            'resources/css/submit-judge-app.css',
+            'resources/css/submit.css',
+            'resources/css/view-apps.css'],
             refresh: true,
         }),
     ],
